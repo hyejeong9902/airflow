10f7dac2cf889090b4edf4d0df93a3a76646a7d7
khj990220 <khj990220@naver.com>
2025-03-20 17:44:47 +0900

    커넥션 테스트용 dag 업로드

--- dags/dag_conn_test.py
+++ dags/dag_conn_test.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id = "dags_conn_test",
-    shedule=None,
+    schedule=None,
     start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:

6f23ac762d83101bbbe2e3c616b1a5235cb05bd0
khj990220 <khj990220@naver.com>
2025-03-20 14:48:49 +0900

    bash_operator dag 업로드

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="dags_bash_operator", # DAG 맨 처음 화면 LIST에서 보이는 부분, 파이썬 파일명과는 상관 없음, 하지만 DAG파일명과 ID는 일치시키는게 좋음
     schedule="0 0 * * *", # 크론 스케줄: dag이 언제도는지 설정
-    start_date=pendulum.datetime(2025, 3, 20, tz="Asia/Seoul"), # dag이 도는 것을 언제 시작할지
+    start_date=pendulum.datetime(2025, 3, 1, tz="Asia/Seoul"), # dag이 도는 것을 언제 시작할지
     catchup=False, # start_date보다 현재 코드작성 시간이 후일때 True로 설정하면 start_time~현재시간 까지 누락된 기간이 한꺼번에 돌아감. 그래서 일반적으로 False
     #dagrun_timeout=datetime.timedelta(minutes=60), # 얼마나 돌아갈 것인지
     #tags=["example", "example2"], # list에서 보이는 tag들, optional

ec9bffa1fc94c955da17ffff713c7b1adb911508
khj990220 <khj990220@naver.com>
2025-03-24 15:14:39 +0900

    공통 외부함수 추가

---
+++ dags/dag_python_import_func.py
@@ -0,0 +1,17 @@
+from airflow import DAG 
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+with DAG(
+    dag_id = "dag_python_import_func",
+    schedule="30 6 * * *", # 매일 6시 30분 마다 수행
+    start_date=pendulum.datetime(2025, 3, 1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    task_get_sftp = PythonOperator(
+        task_id = 'task_get_sftp',
+        python_callable=get_sftp # 이 함수를 돌리겠다는 의미
+    )

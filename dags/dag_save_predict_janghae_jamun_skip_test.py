065350203718ea3c7b736744b610f64c8cd56ce4
khj990220 <khj990220@naver.com>
2025-04-16 13:10:10 +0900

    오퍼레이터 수정

--- dags/dag_save_predict_janghae_jamun_skip_test.py
+++ dags/dag_save_predict_janghae_jamun_skip_test.py
@@ -701,5 +701,4 @@ with DAG(
         op_args=["end predict"]
     )
 
-    #t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
-    t1 >> t2 >> [t3 >> t4 >> t5] >> t6 >> t7
+    t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
